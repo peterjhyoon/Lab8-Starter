a3c5e419f74a4cfd3a6aa51d4e541a6fcefd525b
Peter Yoon <peyoon@ucsd.edu>
2024-05-28 23:22:51 -0700

    expose

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab8-Starter
+
+## Members
+Peter Yoon
+
+## Graceful Degregation
+Graceful Degradation and service workers are related as service workers provide an avenue of applying another layer of Graceful degradation to a web page.
+
+
+## Link to Pages
+pages
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,15 +68,21 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const localRecipes = localStorage.getItem('recipes');
+  if (localRecipes !== null) {
+    return JSON.parse(localRecipes);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const fetchRecipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async (resolve,reject) => {
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
@@ -100,6 +106,21 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+    
+    try {
+      for(const recipeURL of RECIPE_URLS) {
+        const response = await fetch(recipeURL);
+        const jsonData = await response.json();
+        fetchRecipes.push(jsonData);
+        
+      }
+      saveRecipesToStorage(fetchRecipes);
+      resolve(fetchRecipes);
+    } catch(error) {
+      console.error(`Error detected: ${error}`);
+      reject(error);
+    }
+  });
 }
 
 /**
